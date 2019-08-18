939b30724e3e2ee403a85f71041f0beb2a31cf73
Wilfred Vásquez <wilfredvas@gmail.com>
2019-08-18 00:29:29 -0400

    Se realiza la asociación de las rutas con sus clases; por los momentos estas clases retornarán una mensaje. Se crea un archivo de importación postman para testear los endpoint

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,9 @@
 from flask_restful import Api
 # Test function
-from .controller import Test
+from .controller import (
+    Test, User, UserLogin, UserLogout, ConfirmEmail, ChangePassword, ResetPassword,
+    VerifyToken, TokenRefresh, AuthorizeDevice, UserList, SpecificUSer
+)
 
 class RoutesAuth:
     version_base_dir = "/v1"
@@ -11,25 +14,21 @@ class RoutesAuth:
         api = Api(blueprint)
         api.add_resource(Test, "{}/<string:name>".format(cls.auth_base_dir))
         
-        '''
-        These are the project initial routes
+        # --------------------
+        # Path to client user
+        # --------------------
+        api.add_resource(User, '{}/user'.format(cls.auth_base_dir))
+        api.add_resource(UserLogin, '{}/login'.format(cls.auth_base_dir))
+        api.add_resource(UserLogout, '{}/logout'.format(cls.auth_base_dir))
+        api.add_resource(ConfirmEmail, '{}/confirm-email'.format(cls.auth_base_dir))
+        api.add_resource(ChangePassword,'{}/change-password'.format(cls.auth_base_dir))
+        api.add_resource(ResetPassword, '{}/reset-password'.format(cls.auth_base_dir))
+        api.add_resource(VerifyToken, '{}/verify'.format(cls.auth_base_dir))
+        api.add_resource(TokenRefresh, '{}/refresh'.format(cls.auth_base_dir))
+        api.add_resource(AuthorizeDevice, '{}/authorize-device'.format(cls.auth_base_dir))
         
-        --------------------
-        Path to client user
-        --------------------
-        api.add_resource(cls.auth_base_dir+'/user', 'user', hello_world)
-        api.add_resource(cls.auth_base_dir+'/login', 'login', hello_world)
-        api.add_resource(cls.auth_base_dir+'/logout', 'logout', hello_world)
-        api.add_resource(cls.auth_base_dir+'/confirm-email', 'confirm_email', hello_world)
-        api.add_resource(cls.auth_base_dir+'/change-password', 'change_password', hello_world)
-        api.add_resource(cls.auth_base_dir+'/reset-password', 'reset_password', hello_world)
-        api.add_resource(cls.auth_base_dir+'/verify', 'verify', hello_world)
-        api.add_resource(cls.auth_base_dir+'/refresh', 'refresh', hello_world)
-        api.add_resource(cls.auth_base_dir+'/authorize-device', 'authorize_device', hello_world)
-        
-        --------------------
-        Path of admin user
-        --------------------
-        api.add_resource(cls.auth_base_dir+'/users', 'users_list', hello_world)
-        api.add_resource(cls.auth_base_dir+'/user/<int:user_id>', 'specific_user', hello_world)
-        '''
+        # --------------------
+        # Path of admin user
+        # --------------------
+        api.add_resource(UserList, '{}/users'.format(cls.auth_base_dir))
+        api.add_resource(SpecificUSer, '{}/user/<int:user_id>'.format(cls.auth_base_dir))
